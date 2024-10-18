66c8cc189babe677c6e5ffcf16325cbc1cdc4554
umehmictech <iumeh059@gmail.com>
2024-10-18 11:41:47 +0100

    fresh commit

--- nature.js
+++ nature.js
@@ -1,16 +1,47 @@
-//const MOVIE_URL = "https://dummyjson.com/products";
+const MOVIE_URL = 'https://dummyapi.online/api/movies' ;
 
-//const fetchMovies = async () =>{
-   // try{
- //       const response = await fetch (`${MOVIE_URL}`);
-  //      const result = await response.json();
-   //     return result.movies;
-   // } catch (err){
-  //      console.log (err);
-   // }
-//};
-//console.log (fetchMovies())
+const fetchMovies = async () =>{
+    try{
+        const response = await fetch (`${MOVIE_URL}`);
+        const result = await response.json();
+        return result.movies;
+    } 
+    catch (err){
+        console.log (err);
+    }
+};
+console.log (fetchMovies())
 
-fetch('https://dummyapi.online/api/movies')
-    .then((response) => response.json())
-    .then((json) => console.log(json));
+const renderMovies = (movie) => {
+    const movieContainer = document.querySelector('movie-container');
+    movie.forEach( movie => {
+
+        movieDiv = document.createElement('div');
+        movieDiv.classList.add(movie-card);
+
+        const moviePoster = document.createElement('img');
+        moviePoster.src = 'https://image.tmdb.org/t/p/w500/' + movie.poster_path;
+        movieCard.appendChild(moviePoster);
+
+
+        const movieTitle = document.createElement('h1');
+        movieTitle.textContent = movie.title;
+        movieCard.appendChild(movieTitle);
+
+        const movieReleaseDate = document.createElement('p');
+        movieReleaseDate.textContent = 'Release Date: ' + movie.release_date;
+        movieCard.appendChild(movieReleaseDate);
+
+        const moviePlot = document.createElement('p');
+        moviePlot.textContent = movie.overview;
+        movieCard.appendChild(moviePlot);
+
+    } );
+};
+ 
+document.addEventListener("DOMContentLoaded", async() => {
+    const ListOfProducts = await fetchProduct ();
+    renderProduct (ListofProducts);
+})
+
+
